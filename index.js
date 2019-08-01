52e457846e90f6278c16a9f1571f91341502a1e2
bobbidigi <bobbidigi34@gmail.com>
2019-08-01 19:37:17 -0400

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const team = document.querySelector('.team');
+
+window.addEventListener('load', (event) => {
+    // event.target.body.classList.add('bodyAnimation');
+    // setTimeout(function () {
+        // event.target.body.classList.remove("bodyAnimation");
+    // }, 3000);
+});
